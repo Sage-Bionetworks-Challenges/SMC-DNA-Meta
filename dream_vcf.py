a309b40450dfae581729bb94f07a287ef3eee2bc
ShadrielleE <sespiritu5@gmail.com>
2015-06-25 11:03:28 -0400

    Update dream_vcf.py
    Submission file SHOULD now contain a header line listing the pipelines used in the model. It can also include additional header lines if necessary.
    Converted sub.vcf file now has two extra lines as part of metadata: SAMPLE indicating sample name (IS1-4), and Pipelines listing the pipelines used by the contestant.
    Added additional file constraints to accommodate above changes

--- dream_vcf.py
+++ dream_vcf.py
@@ -15,7 +15,7 @@ Ontario Institute for Cancer Research
 '''
 
 # ---------- LOCALS ---------- #
-columns = ('CHROM', 'POS', 'Sample', 'Binary.Cutoff', 'Continuous.Confidence.Score')
+columns = ('#CHROM', 'POS', 'Sample', 'Binary.Cutoff', 'Continuous.Confidence.Score')
 chroms = range(1,23)
 chroms.extend(('X','Y'))
 samples = ['IS1', 'IS2', 'IS3', 'IS4']
@@ -33,24 +33,43 @@ def validate(infile):
 		sys.stderr.write("ERROR: can't find file or read data from: " + infile + "\n")
 		sys.exit(1)
 	
-	first_line = input_fh.readline()
-	fields = first_line.split('\t')
+	# check list of pipelines
+	pipe = re.compile('^##Pipelines=.+')
+	pipelines_str = input_fh.readline()
+	if pipe.match(pipelines_str) == None:
+		sys.stderr.write("ERROR: list of pipelines not found\n")
+		sys.stderr.write("\tFound " + pipelines_str)
+		sys.stderr.write("\tRequires ##Pipelines=XXX1,XXX2,etc.\n")
+		sys.exit(1)
+	
+	# ensure pipelines in correct format
+	pipelines = (pipelines_str.split('=')[1]).split(',')
+
+	skip = re.compile('^##')
+	while(1):
+		sub_columns = input_fh.readline()
+		if skip.match(sub_columns):
+			continue
+		break
+	
+	fields = sub_columns.split('\t')
 	for i in range(len(columns)):
 		try:
 			if fields[i].strip() != columns[i]:
-				print "Invalid column name: " + fields[i]
-				print "Should be: " + columns[i]
+				sys.stderr.write("Invalid column name: " + fields[i] + "\n")
+				sys.stderr.write("Should be: " + columns[i] + "\n")
+				sys.stderr.write("\tCheck whether you intended to have '##' instead of '#'\n")
 				sys.exit(1)
 		except:
 			if columns[i] == 'Continuous.Confidence.Score':
 				print "Optional confidence score not provided."
 			else:
-				print "Missing column: " + columns[i]
+				sys.stderr.write("Missing column: " + columns[i] + "\n")
 				sys.exit(1)
 	
 	print "Submitted file has proper column names.\n"
 	print "Summarizing calls..."
-	
+
 	try:
 		unique_sample = None
 		positive_calls = 0
@@ -60,15 +79,15 @@ def validate(infile):
 			rec_fields = rec.split('\t')
 			# check CHROM
 			if rec_fields[0] not in str(chroms):
-				print "Invalid CHROM: " + rec_fields[0]
-				print "\tValid options: [%s]" % ', '.join(map(str,chroms))
+				sys.stderr.write("Invalid CHROM: " + rec_fields[0] + "\n")
+				sys.stderr.write("\tValid options: [%s]\n" % ', '.join(map(str,chroms)))
 				sys.exit(1)
 
 			# check POS
 			try:
 				pos = int(rec_fields[1])
 			except:
-				print "POS not an int: " + rec_fields[1]
+				sys.stderr.write("POS not an int: " + rec_fields[1] + "\n")
 				sys.exit(1)
 
 			# check sample name
@@ -76,20 +95,20 @@ def validate(infile):
 				unique_sample = rec_fields[2]
 			
 			if rec_fields[2] not in samples:
-				print "Invalid Sample: " + rec_fields[2]
-				print "\tValid options: [%s]" % ', '.join(map(str,samples))
+				sys.stderr.write("Invalid Sample: " + rec_fields[2] + "\n")
+				sys.stderr.write("\tValid options: [%s]\n" % ', '.join(map(str,samples)))
 				sys.exit(1)
 
 			if rec_fields[2] != unique_sample:
-				print "Sample name not unique: " + rec_fields[2]
-				print "\tHave been tracking " + unique_sample
+				sys.stderr.write("Sample name not unique: " + rec_fields[2] + "\n")
+				sys.stderr.write("\tHave been tracking " + unique_sample + "\n")
 				sys.exit(1)
 
 			# check binary cutoff
 			try:
 				binary = int(rec_fields[3])
 			except:
-				print "Binary.Cutoff not an int: " + rec_fields[2]
+				sys.stderr.write("Binary.Cutoff not an int: " + rec_fields[2] + "\n")
 				sys.exit(1)
 
 			if binary == 1:
@@ -97,10 +116,10 @@ def validate(infile):
 			elif binary == 0:
 				negative_calls += 1
 			else:
-				print "Binary cutoff should be 0 or 1: " + rec_fields[3]
+				sys.stderr.write("Binary cutoff should be 0 or 1: " + rec_fields[3] + "\n")
 				sys.exit(1)
 	except:
-		print "\nValidation Failed.\n"
+		sys.stderr.write("\nValidation Failed.\n")
 		sys.exit(1)
 	
 	print "Validation complete.\n"
@@ -119,31 +138,54 @@ def convert(infile, truth):
 	print "Starting Conversion.\n"
 	assert truth.endswith('.vcf') or truth.endswith('.vcf.gz')
 
+	# try opening file before proceeding to process data
 	try:
 		input_fh = open(infile, 'r')
 	except IOError:
 		sys.stderr.write("ERROR: can't find file or read data!\n")
 		sys.exit(1)
-
 	output_fh = open('sub.vcf', 'w')
+
+	# get unique sample name
+	skip = re.compile('^#.*')
+	with open(infile, 'r') as f:
+		while(1):
+			line = f.readline()
+			if skip.match(line):
+				continue
+			unique_sample = line.split('\t')[2]
+			break
 
 	truvcfh = vcf.Reader(filename=truth)
 	# compile list of true records, in case user's output doesn't have calls sorted in order
 	trulist = [trurec for trurec in truvcfh]
 
 	vcf_header = "##fileformat=VCFv4.1\n"
+	vcf_sample = "##SAMPLE=<ID=" + unique_sample + ">\n"
 	vcf_fields = "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\n"
 
+	# get pipelines used
+	pipelines_str = input_fh.readline()
+	while(1):
+		sub_columns = input_fh.readline()
+		if skip.match(sub_columns):
+			next
+		break
+	
+	# writing meta data
 	output_fh.write(vcf_header)
+	output_fh.write(vcf_sample)
+	output_fh.write(pipelines_str)
 	output_fh.write(vcf_fields)
 
-	first_line = input_fh.readline()
+	# evaluating each call
+	sub_columns = input_fh.readline()
 	for line in input_fh:
 		fields = line.split('\t')
 		chrom = fields[0]
 		chrom = chrom.replace("chr","")
 		pos = fields[1]
-		binary = fields[2]
+		binary = fields[3]
 
 		if int(binary):
 			'''
